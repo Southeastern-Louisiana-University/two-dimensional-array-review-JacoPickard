7e9f1677a3e0a16b151e8cb1e2e6377cc54503eb
W0769269 <jacopickard@gmail.com>
2026-02-12 12:16:00 -0600

    2/12 final commit push

--- TwoDimArray/src/Person.java
+++ TwoDimArray/src/Person.java
@@ -1,13 +1,24 @@
 public class Person {
     private String name;
     private int age;
+
     public void main(String[] args){
         System.out.println("Hello Person Exercise");
+        Person person = new Person();
+        person.getAge();
+        System.out.println("Age is " + person.getAge());
+        person.setAge(10);
+        System.out.println("Age is " + person.getAge());
 
     }
-    public Person(String name, int age){
+    public void Person(String name, int age){
         this.name = name;
         this.age = age;
-
     }
+
+    public int getAge(){
+        return age;
+    }
+    public int setAge(){
+        return age;
 }
