70609285b27dfdfe2b83742c31368d80d012d833
W0769269 <jacopickard@gmail.com>
2026-02-12 12:02:42 -0600

    2/12 init commit push

--- TwoDimArray/src/Person.java
+++ TwoDimArray/src/Person.java
@@ -1,11 +1,13 @@
 public class Person {
-    private String main;
+    private String name;
     private int age;
-    public void main(String args[]){
+    public void main(String[] args){
         System.out.println("Hello Person Exercise");
 
     }
     public Person(String name, int age){
+        this.name = name;
+        this.age = age;
 
     }
 }
